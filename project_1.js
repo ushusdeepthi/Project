abad73b42163eddfa8386a149a736aa822bb0c18
Deepthi Ushus <ushusdeepthi@gmail.com>
2020-10-07 13:20:15 +0200

    added new class

--- project_1.js
+++ project_1.js
@@ -6,50 +6,47 @@ document.addEventListener("DOMContentLoaded", function(e) {
  })
 })
 class Player{
-   constructor(){
-        this.name="";
-        this.number_of_questions=0;
-        this.score=0;    
-   } 
-   //Method gets data from the html form and assigns it to the player properties in teh constructor.
-   playerData(){
-    let personal_details=document.getElementById("personal_details");
-    this.name=document.getElementById("name").value;
-    console.log(this.name);
-    this.number_of_questions= document.getElementById("questions").value;
-    console.log(this.number_of_questions);
-    let welcome=document.getElementById("welcome");
-    welcome.remove();
-    personal_details.remove();
-    this.newGame();
-
-   }  
-   //method to fetch data from API and transfer those data to a new Class
-     async newGame(){
-        let header=document.getElementById("header");
-        let player_name= document.createElement("p");
-        player_name.innerHTML="Name:" + this.name;//getting player name from input
-        header.appendChild(player_name)
-        let number_question
-        for (let i=1; i<=this.number_of_questions;i++)//creating no: of buttons= no: of questions player will have
-        {
-            number_question=document.createElement("button");
-            number_question.setAttribute("id","number_question"+i);
-            number_question.innerHTML=i;
-            header.appendChild(number_question);
-        }
-        let start_quiz=document.createElement("button");
-        start_quiz.setAttribute("id","start_quiz");
-       start_quiz.textContent="START QUIZ";
-        header.appendChild(start_quiz);
-        //awaiting for fetch to collect data
-        await fetch('https://quizapi.io/api/v1/questions?apiKey=l3yYIL0YelN5Oto4f09M7Qec0uVJfIHsfh6QuOw6&limit=10')
-        .then(response => response.json())
-        .then(data => {
-
-            let quiz = new Quiz(data);//sending data collected to a new object quiz of class Quiz
-            quiz.loading(this.number_of_questions,this.score);//calling method and sending number of questions and score as parametrs
-        });
-
-    }
-} 
+    constructor(){
+         this.name="";
+         this.number_of_questions=0;
+         this.score=0;    
+    }  
+    playerData(){
+     let personal_details=document.getElementById("personal_details");
+     this.name=document.getElementById("name").value;
+     console.log(this.name);
+     this.number_of_questions= document.getElementById("questions").value;
+     console.log(this.number_of_questions);
+     let welcome=document.getElementById("welcome");
+     welcome.remove();
+     personal_details.remove();
+     this.newGame();
+ 
+    }  
+    async newGame(){
+         let header=document.getElementById("header");
+         let player_name= document.createElement("p");
+         player_name.innerHTML="Name:" + this.name;
+         header.appendChild(player_name)
+         let number_question;
+         for (let i=1; i<=this.number_of_questions;i++)
+         {
+             number_question=document.createElement("button");
+             number_question.setAttribute("id","number_question"+i);
+             number_question.innerHTML=i;
+             header.appendChild(number_question);
+         }
+         let start_quiz=document.createElement("button");
+         start_quiz.setAttribute("id","start_quiz");
+        start_quiz.textContent="START QUIZ";
+         header.appendChild(start_quiz);
+         await fetch('https://quizapi.io/api/v1/questions?apiKey=l3yYIL0YelN5Oto4f09M7Qec0uVJfIHsfh6QuOw6&limit=10')
+         .then(response => response.json())
+         .then(data => {
+ 
+             let quiz = new Quiz(data);
+             quiz.loading(this.number_of_questions,this.score);
+         })
+ 
+                 }
+              }       
--- quiz_1.js
+++ quiz_1.js
@@ -1,4 +1,3 @@
-//Class has all the properties of quiz, question, answers etc.
 class Quiz {
     constructor(data) {
        this.category=[];
@@ -6,7 +5,7 @@ class Quiz {
             this.answer_arr=[];
             this.correct_answer=[];
             this.current_qusetion=0;
-        //pushing data from API into specific arrays
+
         for(let i=0; i<10;i++){
             this.category.push(data[i].category);
             this.question_arr.push(data[i].question);
@@ -15,40 +14,39 @@ class Quiz {
 
         }       
     }
-    //Methos explaining and overview of what to do, when we reach a specific question-DOM manipulation.
+
     loading(number,score){
         console.log(this.category);
         console.log(this.question_arr);
         console.log(this.correct_answer);
         console.log(this.answer_arr);
+
+        
         let start_quiz=document.getElementById("start_quiz");
         start_quiz.addEventListener("click",()=>{  
             start_quiz.textContent="NEXT";
-            start_quiz.className="next";
-            //let a=this.current_qusetion+1;
-            //let b=document.getElementById("number_question"+a);
-            //b.className="trial";
+            start_quiz.className="next";  
             if(this.current_qusetion==0){
                 this.displayQuestion();
             }
             if(this.current_qusetion>0 && this.current_qusetion<number){
                
                 if(this.current_qusetion==(number-1)) start_quiz.textContent="FINISH";
-                let answer=this.chosenAnswer(this.correct_answer);
-                console.log(answer);
-                if(answer===true){
+                let a=this.chosenAnswer(this.correct_answer);
+                console.log(a);
+                if(a===true){
                     score=score+1;
                 }else{
                     score=score+0;}
                 console.log(score);
                  let display_space=document.getElementById("display");
-                display_space.remove();                               
+                display_space.remove();                                
                 this.displayQuestion();
                 }
             if (this.current_qusetion==number){
-                let a=this.chosenAnswer();
-                console.log(a);
-                if(a===true){
+                let answer=this.chosenAnswer();
+                console.log(answer);
+                if(answer===true){
                     score=score+1;
                 }else{
                     score=score+0;}
@@ -56,19 +54,32 @@ class Quiz {
                 let display_space=document.getElementById("display");
                 let start_quiz=document.getElementById("start_quiz");
                 for(let i=1;i<=number;i++){
-                    document.getElementById("number_question"+i).remove()};
-                display_space.remove();                                
-               
+                document.getElementById("number_question"+i).remove()};
+                display_space.remove();                               
                 start_quiz.remove();
                 display_space.remove();
+                let result=document.createElement("div");
+                result.setAttribute("id","result");
+                if(score===number){
+                    let top_score=document.createElement("p");
+                    top_score.innerHTML="CONGRATULATIONS!!! You have the top score"
+                    result.appendChild(top_score);
+                }
+                if(score<=(number/4)){
+                    let top_score=document.createElement("p");
+                    top_score.innerHTML="Your level is below Average"
+                    result.appendChild(top_score);
+                }
                 let score_box=document.createElement("p");
                 score_box.setAttribute("id","score_box");
                 score_box.innerHTML+="TOTAL SCORE :"+ score;
-                let header=document.getElementById("header");
-                header.appendChild(score_box);
+               
+                result.appendChild(score_box);
                 let new_game=document.createElement("button");
                 new_game.textContent="NEW GAME";
-                header.appendChild(new_game);
+                result.appendChild(new_game);
+                let header=document.getElementById("header");
+                header.appendChild(result);
                 new_game.addEventListener("click",function(e){
                    window.location.reload();
                 })   
@@ -76,59 +87,33 @@ class Quiz {
             this.current_qusetion++;    
         })
     }
-        //method to display category, questions & set of answers
         displayQuestion(){
         let header=document.getElementById("header");
         let display_space=document.createElement("div");
         display_space.setAttribute("id","display");
         let question= document.createElement("p");
+        question.className="question";
         let category= document.createElement("p");
+        category.className="category";
         category.textContent="Category: "+this.category[this.current_qusetion];
         question.textContent="Question "+(this.current_qusetion+1)+": "+this.question_arr[this.current_qusetion];
         display_space.appendChild(category);
         display_space.appendChild(question);
-        let checkbox_1=document.createElement("input");
-        checkbox_1.setAttribute("type","checkbox");
-        
-        let answer1=document.createElement("p");
-        answer1.textContent=this.answer_arr[this.current_qusetion].answer_a;
-        display_space.appendChild(answer1);
-        answer1.prepend(checkbox_1);
-        let checkbox_2=document.createElement("input");
-        checkbox_2.setAttribute("type","checkbox")
-        let answer2=document.createElement("p");
-        answer2.textContent=this.answer_arr[this.current_qusetion].answer_b;
-        display_space.appendChild(answer2);
-        answer2.prepend(checkbox_2);
-        if(this.answer_arr[this.current_qusetion].answer_c!=null){
-            let checkbox_3=document.createElement("input");
-            checkbox_3.setAttribute("type","checkbox");
-            let answer3=document.createElement("p");
-            answer3.textContent=this.answer_arr[this.current_qusetion].answer_c;
-            display_space.appendChild(answer3);
-            answer3.prepend(checkbox_3);
-        }
-        if(this.answer_arr[this.current_qusetion].answer_d!=null){
-            let checkbox_4=document.createElement("input");
-            checkbox_4.setAttribute("type","checkbox");
-            let answer4=document.createElement("p");
-            answer4.textContent=this.answer_arr[this.current_qusetion].answer_d;
-            display_space.appendChild(answer4);
-            answer4.prepend(checkbox_4);
-        }
-        if(this.answer_arr[this.current_qusetion].answer_e!=null){
-            let checkbox_5=document.createElement("input");
-            checkbox_5.setAttribute("type","checkbox");
-            let answer5=document.createElement("p");
-            answer5.textContent=this.answer_arr[this.current_qusetion].answer_e;
-            display_space.appendChild(answer5);
-            answer5.prepend(checkbox_5);
-        }
-        header.appendChild(display_space);
-    
-
-    } 
-    //Method gets data from checkboxes, and correct answers to respective arrays
+        let answer=new Answers(this.answer_arr[this.current_qusetion]);
+        console.log(answer.answer);
+        for(let i=0;i<answer.answer.length;i++){
+           if(answer.answer[i]!=null){
+            let checkbox=document.createElement("input");
+            checkbox.setAttribute("type","checkbox");
+            let answer_element=document.createElement("p");
+            answer_element.className="answer";
+            answer_element.textContent=answer.answer[i];
+            display_space.appendChild(answer_element);
+            answer_element.prepend(checkbox);
+           }  
+       }
+       header.appendChild(display_space);    
+    }
     chosenAnswer(){
         
         let checkbox_collection=document.getElementsByTagName("input");
@@ -136,7 +121,9 @@ class Quiz {
         checkbox_collection=Array.from(checkbox_collection);
         console.log(checkbox_collection);
             let checkbox_check=checkbox_collection.map((value)=>{
-                if (value.checked)return true;
+                if (value.checked){
+                    value.className="checked";
+                    return true}
                 else return false;
             });
             
@@ -159,7 +146,6 @@ class Quiz {
         console.log(points);
         return points;
     }
-    //Method taking parameters of correct answer array and checkbox checked and comparing both
     gamePoints(correct_answer_check,checkbox_check){
         console.log(correct_answer_check);
         console.log(checkbox_check);
@@ -175,3 +161,18 @@ class Quiz {
       
     }
 } 
+   
+class Answers{
+    constructor(answer_curr_question){
+        this.answer=this.getAnswer(answer_curr_question);
+    }
+    getAnswer(answer_curr_question){
+        let single_answer_arr=[ answer_curr_question.answer_a,
+                                answer_curr_question.answer_b,
+                                answer_curr_question.answer_c,
+                                answer_curr_question.answer_d,                        
+                                answer_curr_question.answer_e];
+        return single_answer_arr;
+    }
+
+}    
